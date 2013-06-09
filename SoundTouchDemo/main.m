6c2cedc539aeeb7fcfb30eb0d6994f34374d6fea
hejinlai <hejinlai@yunzhisheng.cn>
2013-06-09 14:52:57 +0800

    Initial Commit

---
+++ SoundTouchDemo/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  SoundTouchDemo
+//
+//  Created by hejinlai on 13-6-9.
+//  Copyright (c) 2013年 yunzhisheng. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
